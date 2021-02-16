7c611a3d5a73b94812f0fa0c8908aa9a3d9c2bba
rizwantahir786 <rizwantahir730@gmail.com>
2021-02-16 18:06:01 +0500

    site survey: site info input fields

--- src/views/app-views/pages/Sites/site-profile/site-survey-detail/ClientAndSiteInfo.js
+++ src/views/app-views/pages/Sites/site-profile/site-survey-detail/ClientAndSiteInfo.js
@@ -1,5 +1,5 @@
-import { DeleteOutlined, EyeOutlined, BuildOutlined, UserOutlined, MailOutlined, CompassOutlined, InboxOutlined, PhoneOutlined } from '@ant-design/icons';
-import { Button, Card, Col, DatePicker, Input, Row, Select, Tooltip, Form, Switch } from 'antd';
+import { DeleteOutlined, EyeOutlined, BuildOutlined, UserOutlined, MailOutlined, CompassOutlined, NumberOutlined, PhoneOutlined } from '@ant-design/icons';
+import { Button, Card, Col, DatePicker, Input, Row, Select, Tooltip, Form, Switch, TimePicker, Typography } from 'antd';
 import { Table } from "ant-table-extensions";
 
 import moment from 'moment';
@@ -9,6 +9,7 @@ import { componentStyles } from "./../../styles";
 
 
 const { Option } = Select;
+const { RangePicker } = TimePicker;
 
 
 
@@ -168,8 +169,174 @@ export class ClientAndSiteInfo extends Component {
                                     </div>
                                 </div>
                             </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
 
-                          
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Days</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Number of Guards</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Day Shift</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Night Shift</Typography>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Monday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Tuesday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Wednesday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Thursday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Friday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Saturday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={24} sm={24} md={24} lg={24} >
+
+                                <Row gutter={16}>
+
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+                                        <Typography style={componentStyles.weekdaysStyle}>Sunday</Typography>
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <Input defaultValue={record.phoneNumber} min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<NumberOutlined />} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                    <Col xs={24} sm={24} md={6} lg={6} style={componentStyles.colBorder}>
+
+                                        <RangePicker style={componentStyles.rangePicker} />
+                                    </Col>
+                                </Row>
+                            </Col>
+
+
+
                         </Row>
                         <Row gutter={16} justify="center">
 

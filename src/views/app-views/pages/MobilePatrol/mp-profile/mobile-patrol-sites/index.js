c2f123bed03ce25a08e10c1da4438ec5c31fad85
rizwantahir786 <rizwantahir730@gmail.com>
2021-02-22 16:40:39 +0500

    Mobile Patrol Sites add/edit

--- src/views/app-views/pages/MobilePatrol/mp-profile/mobile-patrol-sites/index.js
+++ src/views/app-views/pages/MobilePatrol/mp-profile/mobile-patrol-sites/index.js
@@ -1,5 +1,5 @@
-import { DeleteOutlined, EyeOutlined } from '@ant-design/icons';
-import { Button, Card, Col, DatePicker, Input, Row, Select, Tooltip } from 'antd';
+import { DeleteOutlined, EyeOutlined, BuildOutlined, CompassOutlined, InboxOutlined, MailOutlined, PhoneOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Card, Col, DatePicker, Input, Row, Select, Tooltip, Form, Switch } from 'antd';
 import { Table } from "ant-table-extensions";
 
 import moment from 'moment';
@@ -39,7 +39,7 @@ const mobilePatrolData = [
 
 const { Option } = Select;
 
-
+const rules = []
 export class MobilePatrolSites extends Component {
 
 	state = {
@@ -48,6 +48,8 @@ export class MobilePatrolSites extends Component {
 			status: "",
 			createDate: "",
 			mpSiteName: "",
+			edit: false,
+			form: false
 		}
 	}
 
@@ -77,7 +79,7 @@ export class MobilePatrolSites extends Component {
 
 
 	render() {
-		const { mobilePatrolList, search } = this.state;
+		const { mobilePatrolList, search, edit, form } = this.state;
 
 		const tableColumns = [
 			{
@@ -85,8 +87,10 @@ export class MobilePatrolSites extends Component {
 				dataIndex: 'mpSiteName',
 				render: (_, record) => (
 					<span className="d-flex">
+						<a onClick={() => this.setState({ edit: true })}>
 
-						{record.mpSiteName}
+							{record.mpSiteName}
+						</a>
 
 					</span>
 				),
@@ -220,12 +224,202 @@ export class MobilePatrolSites extends Component {
 		return (
 			<div style={AppStyles.marginTop50}>
 				<Row gutter={16} justify="center">
+					{edit || form ?
+						<Col xs={24} sm={24} md={24} lg={24} >
+							<Card className="card" title="MP Site Information" style={AppStyles.paddingBottom20}>
+								<Form layout="vertical">
+									<Row gutter={16}>
+										<Col xs={24} sm={24} md={24} lg={24} >
+											<div style={AppStyles.marginBottom40}>
+												<div style={AppStyles.horizontallLineWidth100}>
+												</div>
+											</div>
+										</Col>
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="siteName"
+												label="Site Name"
+												rules={rules.siteName}
+												hasFeedback
+											>
+												<Input style={componentStyles.borderColor} prefix={<BuildOutlined />} />
+											</Form.Item>
+										</Col>
 
-					<Col xs={24} sm={24} md={24} lg={24} style={AppStyles.justifyContentCenter}>
-						<Card className="card" title="Mobile Patrol Sites" >
-							<Table searchable bordered columns={tableColumns} dataSource={mobilePatrolList} rowKey='id' scroll={{ x: 1600, y: 300 }} />
-						</Card>
-					</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="address1"
+												label="Site Address Line 1"
+												rules={rules.address1}
+												hasFeedback
+											>
+												<Input style={componentStyles.borderColor} prefix={<CompassOutlined />} />
+											</Form.Item>
+										</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="address2"
+												label="Site Address Line 2"
+												rules={rules.address2}
+												hasFeedback
+											>
+												<Input style={componentStyles.borderColor} prefix={<CompassOutlined />} />
+											</Form.Item>
+										</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="postcode"
+												label="Post Code"
+												rules={rules.postcode}
+												hasFeedback
+											>
+												<Input type="text" style={componentStyles.borderColor} prefix={<InboxOutlined />} />
+											</Form.Item>
+										</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="city"
+												label="Town / City"
+												rules={rules.city}
+												hasFeedback
+											>
+												<Input style={componentStyles.borderColor} prefix={<CompassOutlined />} />
+											</Form.Item>
+										</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="phone"
+												label="Site Phone"
+												// rules={rules.phone}
+												hasFeedback
+											>
+												<Input min="0" className="remove" type="number" style={componentStyles.borderColor} prefix={<PhoneOutlined />} />
+											</Form.Item>
+										</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="email"
+												label="Site Email"
+												// rules={rules.email}
+												hasFeedback
+											>
+												<Input maxLength={30} style={componentStyles.borderColor} prefix={<MailOutlined />} />
+											</Form.Item>
+										</Col>
+
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="title"
+												label="Title"
+												rules={rules.title}
+												hasFeedback
+											>
+												<Select
+													showSearch
+													style={componentStyles.selectStyle}
+													bordered={false}
+													placeholder="Title"
+													optionFilterProp="children"
+													onChange={(val) => this.handleChange("title", val)}
+													// onFocus={onFocus}
+													// onBlur={onBlur}
+													// onSearch={onSearch}
+													filterOption={(input, option) =>
+														option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+													}
+												>
+													<Option value="Mr">Mr</Option>
+													<Option value="Miss">Miss</Option>
+													<Option value="Mrs">Mrs</Option>
+												</Select>
+											</Form.Item>
+										</Col>
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="fullname"
+												label="Site Contact Full Name"
+												// rules={rules.fullname}
+												hasFeedback
+											>
+												<Input style={componentStyles.borderColor} prefix={<UserOutlined />} />
+											</Form.Item>
+										</Col>
+										<Col xs={24} sm={24} md={8} lg={8}>
+											<Form.Item
+												name="region"
+												label="Region"
+												rules={rules.region}
+												hasFeedback
+											>
+												<Select
+													showSearch
+													style={componentStyles.selectStyle}
+													bordered={false}
+													placeholder="Site Region"
+													optionFilterProp="children"
+													onChange={(val) => this.handleChange("region", val)}
+													// onFocus={onFocus}
+													// onBlur={onBlur}
+													// onSearch={onSearch}
+													filterOption={(input, option) =>
+														option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+													}
+												>
+													<Option value="South East">South East</Option>
+													<Option value="South West">South West</Option>
+												</Select>
+											</Form.Item>
+										</Col>
+										<Col xs={24} sm={24} md={8} lg={8} style={AppStyles.marginTop40}>
+
+
+											<Switch style={componentStyles.switchStyle} size="small" defaultChecked ></Switch>
+	                                        Status										</Col>
+									</Row>
+									<Row gutter={16} justify="center">
+
+										<Col xs={24} sm={24} md={12} lg={12} style={AppStyles.marginTop20}>
+
+											<Form.Item>
+												<Button
+													onClick={() => this.setState({ form: false, edit: false })}
+													style={componentStyles.continueButton} htmlType="submit" block>
+													{form ? "Add Checkpoint" : "Save"}
+												</Button>
+											</Form.Item>
+										</Col>
+									</Row>
+								</Form>
+							</Card>
+
+							{/* <GuardsView data={selectedUser} visible={userProfileVisible} close={()=> {this.closeUserProfile()}}/> */}
+						</Col> :
+						<Col xs={24} sm={24} md={24} lg={24} style={AppStyles.justifyContentCenter}>
+							<Card className="card" title="Mobile Patrol Sites" extra={
+								<Row gutter={16}>
+									<Col xs={24} sm={24} md={24} lg={24}>
+
+										<Button
+											onClick={() => this.setState({ form: true })}
+											style={componentStyles.continueButton} htmlType="submit" block>
+											Add Mobile Patrol Site
+                                                    </Button>
+
+									</Col>
+								</Row>
+							} >
+								<Table searchable bordered columns={tableColumns} dataSource={mobilePatrolList} rowKey='id' scroll={{ x: 1600, y: 300 }} />
+							</Card>
+						</Col>
+
+					}
+
 				</Row>
 
 				{/* <GuardsView data={selectedUser} visible={userProfileVisible} close={()=> {this.closeUserProfile()}}/> */}
